f823526d7821532f468c5391f9d0c9e821abe71f
Dmitry Plevkov <wwarne@gmail.com>
2018-01-12 12:36:18 +0300

    Add function to download webpages

--- cinemas.py
+++ cinemas.py
@@ -1,20 +1,44 @@
+import requests
 
 
-def fetch_afisha_page():
-    pass
+# FETCHING FUCTIONS
+def prepare_session():
+    """
+    The Session object allows us to persist certain parameters across requests.
+    """
+    typical_headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/59.0.3071.115 Safari/537.36',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4'
+    }
+    http_session = requests.Session()
+    http_session.headers.update(typical_headers)
+    return http_session
 
 
-def parse_afisha_list(raw_html):
-    pass
+def fetch_url(url, parameters=None, additional_headers=None, session=None):
+    """
+    Download webpage by its URL.
+    :param url: page url
+    :param parameters: additional GET parameters
+    :param additional_headers: headers for request
+    :param session: which Session object to use
+    :return: request's response object or None in case of errors
+    """
+    session = session or prepare_session()
+    try:
+        webpage_data = session.get(url, params=parameters, headers=additional_headers, timeout=20)
+        webpage_data.raise_for_status()
+    except requests.exceptions.RequestException:
+        # TODO add logging here
+        return None
+    return webpage_data
+
+# VALIDATING FUCTIONS
 
 
-def fetch_movie_info(movie_title):
-    pass
-
-
-def output_movies_to_console(movies):
-    pass
-
+# PARCING FUCTIONS
 
 if __name__ == '__main__':
     pass
